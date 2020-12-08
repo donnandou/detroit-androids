9d66792be9d8aa1b0e5d17d3cf48418702cfff64
Fernando Mansilla <66756575+donnandou@users.noreply.github.com>
2020-12-08 20:55:38 -0300

    Entrega Clase 3 final

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
         <header className="bg-dark">
             <img src="img/logo.png" alt="Detroit Android Store" />
             <nav className="d-flex flex-row justify-content-around align-content-stretch align-items-stretch m-0">
-                <a href="index.html" className="text-decoration-none font-weight-bolder">Home</a>
-                <a href="categories.html" className="text-decoration-none font-weight-bolder">Categorías</a>
-                <a href="about.html" className="text-decoration-none font-weight-bolder">Sobre Nosotros</a>
-                <a href="contacto.html" className="text-decoration-none font-weight-bolder">Contacto</a>
+                <a href="index.html" className="text-decoration-none font-weight-bolder text-light">Home</a>
+                <a href="categories.html" className="text-decoration-none font-weight-bolder text-light">Categorías</a>
+                <a href="about.html" className="text-decoration-none font-weight-bolder text-light">Sobre Nosotros</a>
+                <a href="contacto.html" className="text-decoration-none font-weight-bolder text-light">Contacto</a>
             </nav>
         </header>
     )
